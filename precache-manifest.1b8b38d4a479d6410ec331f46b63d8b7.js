ba66f14531202d377898d36a98d9434ebcc88e05
Moshe Siegel <siegel.moshes@gmail.com>
2019-11-24 14:45:10 -0500

    Updates

---
+++ precache-manifest.1b8b38d4a479d6410ec331f46b63d8b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82f9e4c873ac51acb2832cd1d914dcd",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "97d4fa7674edf6d83d60",
+    "url": "/tic-tac-toe/static/css/main.0e0753ef.chunk.css"
+  },
+  {
+    "revision": "e6c8d611a753392a60f7",
+    "url": "/tic-tac-toe/static/js/2.aaa479a1.chunk.js"
+  },
+  {
+    "revision": "97d4fa7674edf6d83d60",
+    "url": "/tic-tac-toe/static/js/main.f865080a.chunk.js"
+  },
+  {
+    "revision": "25d94bd95813189daa86",
+    "url": "/tic-tac-toe/static/js/runtime-main.00b9c82d.js"
+  }
+]);
